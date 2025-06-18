d81de2f3c2da298d9727e0ae74824a4f97a1135f
gitbeet <nikig715@gmail.com>
2025-06-18 20:11:45 +0200

    typo fix

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -3,7 +3,7 @@ import { describe, it, expect } from "vitest";
 import App from "./App";
 
 describe("App", () => {
-  it("renders the app logo", () => {
+  it("renders the search button", () => {
     render(<App />);
     const searchButton = screen.getByRole("button", { name: "Find My Movie" });
     expect(searchButton).toBeInTheDocument();
