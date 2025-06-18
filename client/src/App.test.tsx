aaed099a9825085bda29dc89a7f9cbb6fe10c57f
gitbeet <nikig715@gmail.com>
2025-06-18 20:10:39 +0200

    vitest setup, basic test

---
+++ client/src/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  it("renders the app logo", () => {
+    render(<App />);
+    const searchButton = screen.getByRole("button", { name: "Find My Movie" });
+    expect(searchButton).toBeInTheDocument();
+  });
+});
